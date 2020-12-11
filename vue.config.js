5422eae9655652cae370e035b66aa573b2182cf0
evan <1137299673@qq.com>
2020-12-12 00:33:49 +0800

    ㊙ add multi-platform build config

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,9 @@ const packageVersion = require('./package.json').version
 const packageName = require('./package.json').name
 const artifactNameInAppId = packageName.replace(/-/g, '_')
 const winIconPath = './public/icons/icon.ico'
+const macIconPath = './public/icons/icon.icns'
+const linuxIconPath = './public/icons/'
+const macEntitlement = './public/entitlements.mac.plist'
 
 module.exports = {
   // multi pages configuration: https://cli.vuejs.org/config/#pages
@@ -34,8 +37,35 @@ module.exports = {
           // waring: releaseNotes will override github release notes. if you want to override github release note, enable it
           // releaseNotes: packageVersion + ' change log'
         },
+        // see https://www.electron.build/configuration/mac
+        mac: {
+          icon: macIconPath,
+          category: 'public.app-category.utilities',
+          entitlements: macEntitlement,
+          entitlementsInherit: macEntitlement,
+          hardenedRuntime: false, // Whether your app has to be signed with hardened runtime.
+          gatekeeperAssess: false // Whether to let electron-osx-sign validate the signing or not.
+        },
+        // see https://www.electron.build/configuration/linux
+        linux: {
+          icon: linuxIconPath,
+          category: 'Utility',
+          target: [
+            {
+              target: 'AppImage',
+              arch: ['x64']
+            }
+          ]
+        },
+        // see https://www.electron.build/configuration/win
         win: {
-          icon: winIconPath
+          icon: winIconPath,
+          target: [
+            {
+              target: 'nsis',
+              arch: ['x64']
+            }
+          ]
         },
         // nsis configuration. see https://www.electron.build/configuration/nsis
         nsis: {
